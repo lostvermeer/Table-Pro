d88874bfe273e4a7553e33b356c0580c79ff8746
Dilshod <d.iadgarov@gmail.com>
2024-10-04 18:43:30 +0300

    React initialization

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './components/App';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import { ThemeProvider } from '@mui/material/styles';
+// import theme from './theme';
+
+const rootElement = document.getElementById('root');
+if (rootElement === null) throw new Error('Root container missing in index.html');
+const root = ReactDOM.createRoot(rootElement!);
+
+root.render(
+//   <ThemeProvider theme={theme}>
+//     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+//     <CssBaseline />
+    <App />
+//   </ThemeProvider>,
+);
+
+
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+
+// import App from './components/app';
+
+// ReactDOM.render(
+//     (<App/>),
+//     document.querySelector('#root')
+// );
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const stylesHandler = 'style-loader';
 
 
 const config = {
-    entry: './src/index.ts',
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
